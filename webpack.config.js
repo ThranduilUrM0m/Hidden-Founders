76df9f37fd131bd47004ea0be8198f1a84d30f59
Boutaleb Zakariae <zakariaeboutaleb@gmail.com>
2017-10-21 22:30:51 +0100

    webpack set up

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+  context: path.join(__dirname, 'src'),
+  entry: [
+    './main.js',
+    './css/main.css',
+  ],
+  output: {
+    path: path.join(__dirname, 'www'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+        ],
+      },
+    ],
+  },
+  resolve: {
+    modules: [
+      path.join(__dirname, 'node_modules'),
+    ],
+  },
+};
