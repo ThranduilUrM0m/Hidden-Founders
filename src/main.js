91653b9bc2b57dfa31b149185e0ea0e866350a13
Boutaleb Zakariae <zakariaeboutaleb@gmail.com>
2017-10-21 22:58:51 +0100

    react added, tried a simple header component no styling

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
 import css from './css/main.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './Header';
 
-console.log('Hello World!');
+document.addEventListener('DOMContentLoaded', function() {
+  ReactDOM.render(
+    React.createElement(Header),
+    document.getElementById('mount')
+  );
+});
